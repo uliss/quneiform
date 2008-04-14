369e6659953b4a39dbae30b79f0e3f9a4e1450b0
JussiP <jpakkane@kosto>
2008-04-14 21:51:57 +0300

    Lowercased header file names.

--- cuneiform_src/Kern/hrk/LineDefs.H
+++ cuneiform_src/Kern/hrk/LineDefs.H
@@ -64,7 +64,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef __LINEDEFS_H
 #define __LINEDEFS_H
 #include "cttypes.h"
-#include "NormDefs.H"
+#include "normdefs.h"
 
 
 typedef enum
