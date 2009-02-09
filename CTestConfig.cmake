45db7f8c9fbd2d6c8e612902d00402d996bf3565
Jussi Pakkanen <jpakkane@kosto>
2009-02-09 14:10:49 +0200

    CTest framework from Dmitry Polevoy.

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+## for CDash
+set( CTEST_PROJECT_NAME       "openocr-core" )
+set( CTEST_NIGHTLY_START_TIME "23:00:00 EDT" )
+set( CTEST_DROP_METHOD        "http" )
+set( CTEST_DROP_SITE          "my.cdash.org" )
+set( CTEST_DROP_LOCATION      "/submit.php?project=openocr-core" )
+set( CTEST_DROP_SITE_CDASH    TRUE )
