8287b7b311ee3f844fbdfe52b91be538703dc5e4
Serge Poltavski <serge.poltavski@gmail.com>
2009-10-02 00:35:39 +0400

    #define => enum, making const

--- cuneiform_src/Kern/leo/alphaset.h
+++ cuneiform_src/Kern/leo/alphaset.h
@@ -1,161 +1,296 @@
 /*
-Copyright (c) 1993-2008, Cognitive Technologies
-All rights reserved.
+ Copyright (c) 1993-2008, Cognitive Technologies
+ All rights reserved.
 
-–азрешаетс€ повторное распространение и использование как в виде исходного кода,
-так и в двоичной форме, с изменени€ми или без, при соблюдении следующих условий:
+ –азрешаетс€ повторное распространение и использование как в виде исходного кода,
+ так и в двоичной форме, с изменени€ми или без, при соблюдении следующих условий:
 
-      * ѕри повторном распространении исходного кода должны оставатьс€ указанное
-        выше уведомление об авторском праве, этот список условий и последующий
-        отказ от гарантий.
-      * ѕри повторном распространении двоичного кода в документации и/или в
-        других материалах, поставл€емых при распространении, должны сохран€тьс€
-        указанна€ выше информаци€ об авторском праве, этот список условий и
-        последующий отказ от гарантий.
-      * Ќи название Cognitive Technologies, ни имена ее сотрудников не могут
-        быть использованы в качестве средства поддержки и/или продвижени€
-        продуктов, основанных на этом ѕќ, без предварительного письменного
-        разрешени€.
+ * ѕри повторном распространении исходного кода должны оставатьс€ указанное
+ выше уведомление об авторском праве, этот список условий и последующий
+ отказ от гарантий.
+ * ѕри повторном распространении двоичного кода в документации и/или в
+ других материалах, поставл€емых при распространении, должны сохран€тьс€
+ указанна€ выше информаци€ об авторском праве, этот список условий и
+ последующий отказ от гарантий.
+ * Ќи название Cognitive Technologies, ни имена ее сотрудников не могут
+ быть использованы в качестве средства поддержки и/или продвижени€
+ продуктов, основанных на этом ѕќ, без предварительного письменного
+ разрешени€.
 
-Ё“ј ѕ–ќ√–јћћј ѕ–≈ƒќ—“ј¬Ћ≈Ќј ¬Ћјƒ≈Ћ№÷јћ» ј¬“ќ–— »’ ѕ–ј¬ »/»Ћ» ƒ–”√»ћ» Ћ»÷јћ» " ј 
-ќЌј ≈—“№" Ѕ≈«  ј ќ√ќ-Ћ»Ѕќ ¬»ƒј √ј–јЌ“»…, ¬џ–ј∆≈ЌЌџ’ я¬Ќќ »Ћ» ѕќƒ–ј«”ћ≈¬ј≈ћџ’,
-¬ Ћё„јя √ј–јЌ“»»  ќћћ≈–„≈— ќ… ÷≈ЌЌќ—“» » ѕ–»√ќƒЌќ—“» ƒЋя  ќЌ –≈“Ќќ… ÷≈Ћ», Ќќ Ќ≈
-ќ√–јЌ»„»¬јя—№ »ћ». Ќ» ¬Ћјƒ≈Ћ≈÷ ј¬“ќ–— »’ ѕ–ј¬ » Ќ» ќƒЌќ ƒ–”√ќ≈ Ћ»÷ќ,  ќ“ќ–ќ≈
-ћќ∆≈“ »«ћ≈Ќя“№ »/»Ћ» ѕќ¬“ќ–Ќќ –ј—ѕ–ќ—“–јЌя“№ ѕ–ќ√–јћћ”, Ќ» ¬  ќ≈ћ —Ћ”„ј≈ Ќ≈
-Ќ≈—®“ ќ“¬≈“—“¬≈ЌЌќ—“», ¬ Ћё„јя ЋёЅџ≈ ќЅў»≈, —Ћ”„ј…Ќџ≈, —ѕ≈÷»јЋ№Ќџ≈ »Ћ»
-ѕќ—Ћ≈ƒќ¬ј¬Ў»≈ ”Ѕџ“ », —¬я«јЌЌџ≈ — »—ѕќЋ№«ќ¬јЌ»≈ћ »Ћ» ѕќЌ≈—≈ЌЌџ≈ ¬—Ћ≈ƒ—“¬»≈
-Ќ≈¬ќ«ћќ∆Ќќ—“» »—ѕќЋ№«ќ¬јЌ»я ѕ–ќ√–јћћџ (¬ Ћё„јя ѕќ“≈–» ƒјЌЌџ’, »Ћ» ƒјЌЌџ≈,
-—“ј¬Ў»≈ Ќ≈√ќƒЌџћ», »Ћ» ”Ѕџ“ » »/»Ћ» ѕќ“≈–» ƒќ’ќƒќ¬, ѕќЌ≈—≈ЌЌџ≈ »«-«ј ƒ≈…—“¬»…
-“–≈“№»’ Ћ»÷ »/»Ћ» ќ“ ј«ј ѕ–ќ√–јћћџ –јЅќ“ј“№ —ќ¬ћ≈—“Ќќ — ƒ–”√»ћ» ѕ–ќ√–јћћјћ»,
-Ќќ Ќ≈ ќ√–јЌ»„»¬јя—№ Ё“»ћ» —Ћ”„јяћ»), Ќќ Ќ≈ ќ√–јЌ»„»¬јя—№ »ћ», ƒј∆≈ ≈—Ћ» “ј ќ…
-¬Ћјƒ≈Ћ≈÷ »Ћ» ƒ–”√ќ≈ Ћ»÷ќ ЅџЋ» »«¬≈ў≈Ќџ ќ ¬ќ«ћќ∆Ќќ—“» “ј »’ ”Ѕџ“ ќ¬ » ѕќ“≈–№.
+ Ё“ј ѕ–ќ√–јћћј ѕ–≈ƒќ—“ј¬Ћ≈Ќј ¬Ћјƒ≈Ћ№÷јћ» ј¬“ќ–— »’ ѕ–ј¬ »/»Ћ» ƒ–”√»ћ» Ћ»÷јћ» " ј 
+ ќЌј ≈—“№" Ѕ≈«  ј ќ√ќ-Ћ»Ѕќ ¬»ƒј √ј–јЌ“»…, ¬џ–ј∆≈ЌЌџ’ я¬Ќќ »Ћ» ѕќƒ–ј«”ћ≈¬ј≈ћџ’,
+ ¬ Ћё„јя √ј–јЌ“»»  ќћћ≈–„≈— ќ… ÷≈ЌЌќ—“» » ѕ–»√ќƒЌќ—“» ƒЋя  ќЌ –≈“Ќќ… ÷≈Ћ», Ќќ Ќ≈
+ ќ√–јЌ»„»¬јя—№ »ћ». Ќ» ¬Ћјƒ≈Ћ≈÷ ј¬“ќ–— »’ ѕ–ј¬ » Ќ» ќƒЌќ ƒ–”√ќ≈ Ћ»÷ќ,  ќ“ќ–ќ≈
+ ћќ∆≈“ »«ћ≈Ќя“№ »/»Ћ» ѕќ¬“ќ–Ќќ –ј—ѕ–ќ—“–јЌя“№ ѕ–ќ√–јћћ”, Ќ» ¬  ќ≈ћ —Ћ”„ј≈ Ќ≈
+ Ќ≈—®“ ќ“¬≈“—“¬≈ЌЌќ—“», ¬ Ћё„јя ЋёЅџ≈ ќЅў»≈, —Ћ”„ј…Ќџ≈, —ѕ≈÷»јЋ№Ќџ≈ »Ћ»
+ ѕќ—Ћ≈ƒќ¬ј¬Ў»≈ ”Ѕџ“ », —¬я«јЌЌџ≈ — »—ѕќЋ№«ќ¬јЌ»≈ћ »Ћ» ѕќЌ≈—≈ЌЌџ≈ ¬—Ћ≈ƒ—“¬»≈
+ Ќ≈¬ќ«ћќ∆Ќќ—“» »—ѕќЋ№«ќ¬јЌ»я ѕ–ќ√–јћћџ (¬ Ћё„јя ѕќ“≈–» ƒјЌЌџ’, »Ћ» ƒјЌЌџ≈,
+ —“ј¬Ў»≈ Ќ≈√ќƒЌџћ», »Ћ» ”Ѕџ“ » »/»Ћ» ѕќ“≈–» ƒќ’ќƒќ¬, ѕќЌ≈—≈ЌЌџ≈ »«-«ј ƒ≈…—“¬»…
+ “–≈“№»’ Ћ»÷ »/»Ћ» ќ“ ј«ј ѕ–ќ√–јћћџ –јЅќ“ј“№ —ќ¬ћ≈—“Ќќ — ƒ–”√»ћ» ѕ–ќ√–јћћјћ»,
+ Ќќ Ќ≈ ќ√–јЌ»„»¬јя—№ Ё“»ћ» —Ћ”„јяћ»), Ќќ Ќ≈ ќ√–јЌ»„»¬јя—№ »ћ», ƒј∆≈ ≈—Ћ» “ј ќ…
+ ¬Ћјƒ≈Ћ≈÷ »Ћ» ƒ–”√ќ≈ Ћ»÷ќ ЅџЋ» »«¬≈ў≈Ќџ ќ ¬ќ«ћќ∆Ќќ—“» “ј »’ ”Ѕџ“ ќ¬ » ѕќ“≈–№.
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice,
-      this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice,
-      this list of conditions and the following disclaimer in the documentation
-      and/or other materials provided with the distribution.
-    * Neither the name of the Cognitive Technologies nor the names of its
-      contributors may be used to endorse or promote products derived from this
-      software without specific prior written permission.
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ * Neither the name of the Cognitive Technologies nor the names of its
+ contributors may be used to endorse or promote products derived from this
+ software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#define	ALPH_ALL 	0x00
-#define	ALPH_RUS 	0x01
-#define	ALPH_DIG	0x02
-#define	ALPH_ROM	0x04
-#define	ALPH_R_E	0x08
-#define	ALPH_ENG    0x10
-#define	ALPH_RALL   0x20
+enum alph_t {
+	ALPH_ALL = 0x00,
+	ALPH_RUS = 0x01,
+	ALPH_DIG = 0x02,
+	ALPH_ROM = 0x04,
+	ALPH_R_E = 0x08,
+	ALPH_ENG = 0x10,
+	ALPH_RALL = 0x20
+};
 //
 // 1 - Capital Russian letters
 // 2 - Digital
 // 4 - Roma digit
 //
-static uchar typ_of_alpha[256] = {
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//                                                         last 0f
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//                                                         last 1f
-     0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,
-//     ! " # $ % & ' ( ) * + , - . /           last = 2f
-     2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,
-//   0 1 2 3 4 5 6 7 8 9 : ; < = > ? @         last = 40
-     8,8,8,8,8,8,8,8,4,8,8,8,8,8,8,8,8,8,8,8,8,4,8,4,8,8,0,0,0,0,0,0,
-//   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ `                                 last: 60
-     8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,0,0,0,0,0,
-//   a b c d e f g h i j k l m n o p q r s t u v w x y z
-     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-//   А Б В Г Д Е Ж З И Й К Л М Н О П
-     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-//   Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я
-     32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,
-//   †  °  Ґ  £  §  •  ¶  І  ®  ©  ™  Ђ  ђ  ≠  Ѓ  ѓ
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+static const uchar typ_of_alpha[256] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0,
+		0,
+		//                                                         last 0f
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0,
+		//                                                         last 1f
+		0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1,
+		//     ! " # $ % & ' ( ) * + , - . /           last = 2f
+		2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0,
+		0,
+		//   0 1 2 3 4 5 6 7 8 9 : ; < = > ? @         last = 40
+		8, 8, 8, 8, 8, 8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 4, 8, 4,
+		8, 8, 0, 0, 0, 0, 0, 0,
+		//   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ `                                 last: 60
+		8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+		8, 8, 0, 0, 0, 0, 0,
+		//   a b c d e f g h i j k l m n o p q r s t u v w x y z
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		//   А Б В Г Д Е Ж З И Й К Л М Н О П
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		//   Р С Т У Ф Х Ц Ч Ш Щ Ъ Ы Ь Э Ю Я
+		32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+		//   †  °  Ґ  £  §  •  ¶  І  ®  ©  ™  Ђ  ђ  ≠  Ѓ  ѓ
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//
+		32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
+		//   а  б  в  г  д  е  ж  з  и  й  к  л  м  н  о  п
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 //
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//
-     32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,
-//   а  б  в  г  д  е  ж  з  и  й  к  л  м  н  о  п
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-//
-   };
+		};
 
+static const uchar up_of_alpha[256] = {
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		//                                                         last 0f
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		//                                                         last 1f
+		0,
+		0,
+		0,
+		'#',
+		0,
+		'%',
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		'/',
+		//     ! "  #  $  %  & ' ( ) * + , - . /           last = 2f
+		(uchar) '0',
+		(uchar) '1',
+		(uchar) '2',
+		(uchar) '3',
+		(uchar) '4',
+		(uchar) '5',
+		(uchar) '6',
+		(uchar) '7',
+		(uchar) '8',
+		(uchar) '9',
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		//    0   1   2   3   4   5   6   7   8   9 : ; <  =  >  ?  @         last = 40
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		'O', 'P', 'Q', 'R',
+		'S',
+		'T',
+		'U',
+		'V',
+		'W',
+		'X',
+		'Y',
+		'Z',
+		0,
+		0,
+		0,
+		0,
+		0,
+		0,
+		//   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [  \  ] ^ _ `                                 last: 60
+		'a', 'b', 'C', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+		'O', 'P', 'q', 'r', 'S',
+		't',
+		'u',
+		'V',
+		'W',
+		'X',
+		'Y',
+		'Z',
+		0,
+		(uchar) '|',
+		0,
+		0,
+		0,
+		//   a b c d e f g h i j k l m n o p q r s t u v w x y z {  |  } ~
+		(uchar) 'А', (uchar) 'Б', (uchar) 'В', (uchar) 'Г', (uchar) 'Д',
+		(uchar) 'Е',
+		(uchar) 'Ж',
+		(uchar) 'З',
+		(uchar) 'И',
+		(uchar) 'Й',
+		(uchar) 'К',
+		(uchar) 'Л',
+		(uchar) 'М',
+		(uchar) 'Н',
+		(uchar) 'О',
+		(uchar) 'П',
+		//    А   Б   В   Г   Д   Е   Ж   З   И   Й   К   Л   М   Н   О   П
+		(uchar) 'Р', (uchar) 'С', (uchar) 'Т', (uchar) 'У', (uchar) 'Ф',
+		(uchar) 'Х', (uchar) 'Ц', (uchar) 'Ч', (uchar) 'Ш',
+		(uchar) 'Щ',
+		(uchar) 'Ъ',
+		(uchar) 'Ы',
+		(uchar) 'Ь',
+		(uchar) 'Э',
+		(uchar) 'Ю',
+		(uchar) 'Я',
+		//    Р   С   Т   У   Ф   Х   Ц   Ч   Ш   Щ   Ъ   Ы   Ь   Э   Ю   Я
+		(uchar) '†', (uchar) '°', (uchar) 'В', (uchar) 'Г', (uchar) 'Д',
+		(uchar) '•', (uchar) 'Ж', (uchar) 'З', (uchar) 'И', (uchar) 'Й',
+		(uchar) 'К', (uchar) 'Л', (uchar) 'М',
+		(uchar) 'Н',
+		(uchar) 'О',
+		(uchar) 'П',
+		//    †   °   Ґ   £   §   •   ¶   І   ®   ©   ™   Ђ   ђ   ≠   Ѓ   ѓ
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		//
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		//
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0,
+		0,
+		//
+		(uchar) 'Р', (uchar) 'С', (uchar) 'Т', (uchar) 'У', (uchar) 'Ф',
+		(uchar) 'Х', (uchar) 'Ц', (uchar) 'Ч', (uchar) 'Ш', (uchar) 'Щ',
+		(uchar) 'Ъ', (uchar) 'Ы', (uchar) 'Ь', (uchar) 'Э', (uchar) 'Ю',
+		(uchar) 'Я',
+		//    а   б   в   г   д   е   ж   з   и   й   к   л   м   н   о   п
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+//
+		};
 
-static uchar up_of_alpha[256] = {
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//                                                         last 0f
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//                                                         last 1f
-     0,0,0,'#',0,'%',0,0,0,0,0,0,0,0,0,'/',
-//     ! "  #  $  %  & ' ( ) * + , - . /           last = 2f
-     (uchar)'0',(uchar)'1',(uchar)'2',(uchar)'3',(uchar)'4',(uchar)'5',(uchar)'6',(uchar)'7',(uchar)'8',(uchar)'9',0,0, 0,0, 0, 0, 0,
-//    0   1   2   3   4   5   6   7   8   9 : ; <  =  >  ?  @         last = 40
-     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 0,0, 0,0,0,0,
-//   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [  \  ] ^ _ `                                 last: 60
-     'a','b','C','d','e','f','g','h','i','j','k','l','m','n','O','P','q','r','S','t','u','V','W','X','Y','Z',0,(uchar)'|',0,0,0,
-//   a b c d e f g h i j k l m n o p q r s t u v w x y z {  |  } ~
-     (uchar)'А',(uchar)'Б',(uchar)'В',(uchar)'Г',(uchar)'Д',(uchar)'Е',(uchar)'Ж',(uchar)'З',(uchar)'И',(uchar)'Й',(uchar)'К',(uchar)'Л',(uchar)'М',(uchar)'Н',(uchar)'О',(uchar)'П',
-//    А   Б   В   Г   Д   Е   Ж   З   И   Й   К   Л   М   Н   О   П
-     (uchar)'Р',(uchar)'С',(uchar)'Т',(uchar)'У',(uchar)'Ф',(uchar)'Х',(uchar)'Ц',(uchar)'Ч',(uchar)'Ш',(uchar)'Щ',(uchar)'Ъ',(uchar)'Ы',(uchar)'Ь',(uchar)'Э',(uchar)'Ю',(uchar)'Я',
-//    Р   С   Т   У   Ф   Х   Ц   Ч   Ш   Щ   Ъ   Ы   Ь   Э   Ю   Я
-     (uchar)'†',(uchar)'°',(uchar)'В',(uchar)'Г',(uchar)'Д',(uchar)'•',(uchar)'Ж',(uchar)'З',(uchar)'И',(uchar)'Й',(uchar)'К',(uchar)'Л',(uchar)'М',(uchar)'Н',(uchar)'О',(uchar)'П',
-//    †   °   Ґ   £   §   •   ¶   І   ®   ©   ™   Ђ   ђ   ≠   Ѓ   ѓ
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+static const uchar leo_accepted[256] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0,
+		0,
+		//                                                         last 0f
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0,
+		//                                                         last 1f
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0,
+		//     !  " #  $  % & ' (   ) *  + , - . /           last = 2f
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0,
+		0,
+		//   0 1 2 3 4 5 6 7 8 9 : ; <  =  >  ?  @         last = 40
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 0,
+		//   A B C D E F G H I J  K L M N O P  Q  R S T U V  W  X Y Z [  \  ] ^ _ `                                 last: 60
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 1, 0, 0, 0, 0, 0,
+		//   a b c d e f  g  h i j k l m n o p  q r s t  u v w x y z
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		//   А Б В Г Д  Е Ж З И Й К Л М Н О П
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		//   Р С Т У Ф Х Ц  Ч Ш Щ  Ъ Ы Ь Э Ю Я
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		//   ° Ґ £ §  • ¶ І ® ©  ™ Ђ ђ ≠ Ѓ ѓ
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		//   а б в г д е ж  з и й  к л м н о п
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 //
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//
-     (uchar)'Р',(uchar)'С',(uchar)'Т',(uchar)'У',(uchar)'Ф',(uchar)'Х',(uchar)'Ц',(uchar)'Ч',(uchar)'Ш',(uchar)'Щ',(uchar)'Ъ',(uchar)'Ы',(uchar)'Ь',(uchar)'Э',(uchar)'Ю',(uchar)'Я',
-//    а   б   в   г   д   е   ж   з   и   й   к   л   м   н   о   п
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-//
-   };
-
-static uchar leo_accepted[256]={
-  0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//                                                         last 0f
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//                                                         last 1f
-     0,0, 0, 0, 0,0,0,0, 0, 0,0, 0,0,0,0, 0,
-//     !  " #  $  % & ' (   ) *  + , - . /           last = 2f
-     1,1,1,1,1,1,1,1,1,1,0,0, 0,0, 0, 0, 0,
-//   0 1 2 3 4 5 6 7 8 9 : ; <  =  >  ?  @         last = 40
-     1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1, 1, 1,1,1,1,1, 1,1, 1,1,1, 0,0, 0,0,0,0,
-//   A B C D E F G H I J  K L M N O P  Q  R S T U V  W  X Y Z [  \  ] ^ _ `                                 last: 60
-     1,1,1,1,1, 1, 1,1,1,1,1,1,1,1,1, 1,1,1,1, 1,1,1,1,1,1, 1,0,0,0,0,0,
-//   a b c d e f  g  h i j k l m n o p  q r s t  u v w x y z
-     1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,
-//   А Б В Г Д  Е Ж З И Й К Л М Н О П
-     1,1,1,1,1,1, 1,1,1, 1,1,1,1,1,1, 1,
-//   Р С Т У Ф Х Ц  Ч Ш Щ  Ъ Ы Ь Э Ю Я
-     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-//   ° Ґ £ §  • ¶ І ® ©  ™ Ђ ђ ≠ Ѓ ѓ
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-//
-     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-//
-     1,1,1,1,1,1, 1,1,1, 1,1,1,1,1,1,1,
-//   а б в г д е ж  з и й  к л м н о п
-     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-//
-};
+		};
