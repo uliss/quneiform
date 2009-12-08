f18af7a4ec9ea646275995b326233e742f4edc6a
Serge Poltavski <serge.poltavski@gmail.com>
2009-12-08 03:02:33 +0300

    RMARKER_Init and _Done cleanup

--- src/rmarker/dll.cpp
+++ src/rmarker/dll.cpp
@@ -117,88 +117,24 @@ Bool32 RMARKER_SearchTableInZone(Handle hPage, Handle hCCOM, uint32_t perc, Rect
 
 Bool32 RMARKER_Init(uint16_t wHeightCode, Handle hStorage)
 {
-    LDPUMA_Init(0, NULL);
-    /*------------------------------------------------------------------------------------------*/
     RNEG_Init(wHeightCode, NULL);
-    /*------------------------------------------------------------------------------------------*/
 #ifdef _USE_RFILL_
 
-    if (!RFILL_Init()) {
-        uint32_t retCode = RFILL_GetReturnCode ();
-        char *pszMsg = RFILL_GetReturnString (retCode);
-        return FALSE;
-    }
+    if (!RFILL_Init())
+    return FALSE;
 
 #endif //_USE_RFILL_
-    /*------------------------------------------------------------------------------------------*/
-    LDPUMA_Registry(&hMainTime, SNAP_ROOT_MAIN_TIME_CONTROL, NULL);
-    LDPUMA_Registry(&hPrep, "Подготовка.Верт. лин. проц.", hMainTime);
-    LDPUMA_Registry(&hPicture, "Выделение трив. картинок.", hMainTime);
-    LDPUMA_Registry(&hNegative, "Выделение негативов..", hMainTime);
-    LDPUMA_Registry(&hTableZones, "Выделение зон таблиц..", hMainTime);
-    LDPUMA_Registry(&hLines3, "Линии.Третий проход..", hMainTime);
-    LDPUMA_Registry(&hVCutInZones, "Верт. резка в ЗТ.", hMainTime);
-    LDPUMA_Registry(&hSearchTables, "Поиск таблиц..", hMainTime);
-    LDPUMA_Registry(&hAnalTables, "Анализ структуры таблиц..", hMainTime);
-    LDPUMA_Registry(&hSVLP, "Верт. лин. проц.", hMainTime);
-    LDPUMA_Registry(&hBlocks, "Выделение фрагментов..", hMainTime);
-    LDPUMA_Registry(&hNegaTestCells, "Нег. диаг. ячеек таблиц..", hMainTime);
-    LDPUMA_Registry(&hVertTestCells, "Верт. диаг. ячеек таблиц..", hMainTime);
-    LDPUMA_Registry(&hFon, "Поис фона..", hMainTime);
-    LDPUMA_Registry(&hEnd, "От RMARKER до выделения строк..", hMainTime);
-    LDPUMA_Registry(&hSegment, "Грубое выделение фрагментов...", hMainTime);
-    LDPUMA_Registry(&MainDebug, SNAP_ROOT_MAIN_DEBUG, NULL);
-    // LDPUMA_Registry (&hUseCLine,"Работа с контейнером линий",NULL);
-    LDPUMA_Registry(&DebugFill, "Поиск фона в таблицах", MainDebug);
-    LDPUMA_RegistryHelp(DebugFill, "Активизация вершины разрешит поиск фона в таблицах.", FALSE);
-    LDPUMA_Registry(&hDebugNeg, "Этап поиска негативов.", NULL);
-    LDPUMA_Registry(&hDebugLinePass3, "Этап оценки линий.", NULL);
-    LDPUMA_Registry(&hDebugLinePass2, "Этап третьего прохода по линиям.", NULL);
-    LDPUMA_Registry(&hDebugVerifLine, "Верификация линий.", NULL);
-    LDPUMA_Registry(&hNegaCellsAuto, "Диагностика негативных ячеек таблиц", MainDebug);
-    LDPUMA_RegistryHelp(hNegaCellsAuto,
-            "Активизация вершины разрешит диагностику негативных ячеек таблиц.", FALSE);
-    LDPUMA_Registry(&hVertCellsAuto, "Этап автоматической вертикальной диагностики ячеек таблиц.",
-            NULL);
-    LDPUMA_Registry(&hVertCells, "Работа с вертикальными ячейками таблиц", NULL);
-    LDPUMA_Registry(&hPrintFileVertCells, "Печать в файл вертикальных ячеек", hVertCells);
-    LDPUMA_RegistryHelp(hPrintFileVertCells,
-            "Разрешить печать в файл vertcell.res вертикальных ячеек таблиц.", FALSE);
-    LDPUMA_Registry(&hNegaCells, "Работа с негативными ячейками таблиц", NULL);
-    LDPUMA_Registry(&hDebugUpDown, "Сверху вниз", hVertCells);
-    LDPUMA_RegistryHelp(hDebugUpDown,
-            "При продавлении вершины вертикальные строки будут считаться типа Сверху-Вниз.", FALSE);
-    LDPUMA_Registry(&hDebugPictures, SNAP_LAYOUT_PICTURES, NULL);
-    LDPUMA_Registry(&hLayout, SNAP_ROOT_LAYOUT, NULL);
-    LDPUMA_RegistryHelp(hZone, "Вершина поиска зон таблиц.", FALSE);
-    LDPUMA_Registry(&hPrintCrossedPics, "Отпись пересекаемых таблицами картинок", hLayout);
-    LDPUMA_RegistryHelp(hPrintCrossedPics,
-            "Печать в файл crossed.txt координат картинок, пересекаеющи зоны таблиц", FALSE);
-    LDPUMA_Registry(&hNoRazrTabl, "Отменить разрезание склеенных таблиц.", hZone);
-    LDPUMA_RegistryHelp(hNoRazrTabl, "Отменить разрезание склеенных таблиц.", FALSE);
-    LDPUMA_Registry(&hNoGiveNeg, "Не сдавать негативы.", hZone);
-    LDPUMA_RegistryHelp(hNoGiveNeg, "Не сдавать негативы.", FALSE);
-    LDPUMA_Registry(&hNoSeePict, "Не учитывать картинки.", hZone);
-    LDPUMA_RegistryHelp(hNoSeePict, "Не учитывать картинки.", FALSE);
-    remove("crossed.txt");
     return TRUE;
 }
 
 Bool32 RMARKER_Done()
 {
-    /*------------------------------------------------------------------------------------------*/
     RNEG_Done();
-    /*------------------------------------------------------------------------------------------*/
 #ifdef _USE_RFILL_
 
-    if (!RFILL_Done()) {
-        uint32_t retCode = RFILL_GetReturnCode ();
-        char *pszMsg = RFILL_GetReturnString (retCode);
-        return FALSE;
-    }
+    if (!RFILL_Done())
+    return FALSE;
 
 #endif //_USE_RFILL_
-    /*------------------------------------------------------------------------------------------*/
-    LDPUMA_Done();
     return TRUE;
 }
