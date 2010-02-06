dddf2c1281166781a8e67a20da7fa15d813b59d5
Serge Poltavski <serge.poltavski@gmail.com>
2010-02-06 09:01:33 +0300

    OpenBSD linking fix

--- install_files.cmake
+++ install_files.cmake
@@ -10,6 +10,22 @@ else()
     set(LIBDIR "lib")
 endif()
 
+if(CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
+    if(EXISTS /usr/include/dlfcn.h)
+        SET(CMAKE_DL_LIBS "")
+        SET(CMAKE_SHARED_LIBRARY_C_FLAGS "-fPIC")            # -pic 
+        SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "-shared")       # -shared
+        SET(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")         # +s, flag for exe link to use shared lib
+        SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG "-Wl,-rpath,")       # -rpath
+        SET(CMAKE_SHARED_LIBRARY_RUNTIME_C_FLAG_SEP ":")   # : or empty
+        SET(CMAKE_SHARED_LIBRARY_RPATH_LINK_C_FLAG "-Wl,-rpath-link,")
+        SET(CMAKE_SHARED_LIBRARY_SONAME_C_FLAG "-Wl,-soname,")
+        SET(CMAKE_SHARED_LIBRARY_SONAME_CXX_FLAG "-Wl,-soname,")
+        SET(CMAKE_EXE_EXPORTS_C_FLAG "-Wl,--export-dynamic")
+        SET(CMAKE_EXE_EXPORTS_CXX_FLAG "-Wl,--export-dynamic")
+     endif()
+endif()
+
 macro(library_hook name)
   set_target_properties(${name} PROPERTIES VERSION
   "${CF_VERSION_MAJOR}.${CF_VERSION_MINOR}.${CF_VERSION_PATCH}")
