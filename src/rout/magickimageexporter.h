1f26a32db5c4c54b9bc59d9cef038291d771b9fc
Serge Poltavski <serge.poltavski@gmail.com>
2010-02-23 01:00:33 +0300

    Added skeleton for MagickImageExporter class

---
+++ src/rout/magickimageexporter.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Serge Poltavsky                                 *
+ *   serge.poltavski@gmail.com                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#ifndef MAGICKIMAGEEXPORTER_H_
+#define MAGICKIMAGEEXPORTER_H_
+
+#include "imageexporter.h"
+
+namespace CIF
+{
+
+class MagickImageExporter: public ImageExporter
+{
+    public:
+        virtual ~MagickImageExporter();
+        MagickImageExporter();
+};
+
+}
+
+#endif /* MAGICKIMAGEEXPORTER_H_ */
