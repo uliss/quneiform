e10ffcb038cef12785ec94ead807d17eeef39aa4
Serge Poltavski <serge.poltavski@gmail.com>
2010-08-24 15:01:51 +0400

    Release flag fix

--- cmake/flags_release.cmake
+++ cmake/flags_release.cmake
@@ -1,7 +1,7 @@
 if(CMAKE_BUILD_TYPE STREQUAL Release)
     add_definitions(-DNDEBUG)
     if(CMAKE_SYSTEM_NAME STREQUAL Linux)
-        SET_COMPILER_DEBUG_FLAG(-O2)
+
     endif()
 
     message(STATUS "Compiler flags are: ${CMAKE_CXX_FLAGS_RELEASE}")
