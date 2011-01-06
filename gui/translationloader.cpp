712e6c55fc23817b8653078c3775b5a3774960df
Serge Poltavski <serge.poltavski@gmail.com>
2011-01-06 13:44:14 +0300

    TranslationLoader class added

---
+++ gui/translationloader.cpp
@@ -0,0 +1,101 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Serge Poltavsky                                 *
+ *   serge.poltavski@gmail.com                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>   *
+ ***************************************************************************/
+
+#include <locale.h>
+#include <QLocale>
+#include <QLibraryInfo>
+#include <QDebug>
+
+#ifdef Q_WS_MAC
+#include <CoreFoundation/CFURL.h>
+#include <CoreFoundation/CFBundle.h>
+#endif
+
+#include "config.h" // for INSTALL_DATADIR
+#include "translationloader.h"
+
+// static members init
+QTranslator TranslationLoader::app_;
+QTranslator TranslationLoader::system_;
+
+TranslationLoader::TranslationLoader()
+{
+}
+
+QTranslator * TranslationLoader::applicationTranslator() {
+    return &app_;
+}
+
+void TranslationLoader::load() {
+    loadSystemTranslation();
+    loadApplicationTranslation();
+}
+
+void TranslationLoader::loadSystemTranslation() {
+    qDebug() << Q_FUNC_INFO << "loading qt system translations for locale:" << locale_.name();
+    bool res = system_.load("qt_" + locale_.name(),
+                 QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+
+    if(!res)
+        qDebug() << Q_FUNC_INFO << "unable to load translation for locale:" << locale_.name();
+}
+
+void TranslationLoader::loadApplicationTranslation() {
+    QStringList paths;
+#ifdef Q_WS_MAC
+    CFURLRef appUrlRef = CFBundleCopyBundleURL(CFBundleGetMainBundle());
+    CFStringRef macPath = CFURLCopyFileSystemPath(appUrlRef,
+                                                kCFURLPOSIXPathStyle);
+    const char * pathPtr = CFStringGetCStringPtr(macPath,
+                                                CFStringGetSystemEncoding());
+    CFRelease(appUrlRef);
+    CFRelease(macPath);
+
+    paths << QString(pathPtr) + QString("/Contents/Resources");
+    paths << "gui";
+#else
+    paths << INSTALL_DATADIR "/locale";
+    paths << "/usr/share/cuneiform/locale";
+    paths << "gui";
+#endif
+
+    loadApplicationTranslations(paths);
+}
+
+bool TranslationLoader::loadApplicationTranslation(const QString& path) {
+    QString tr_name = locale_.name();
+    bool res = app_.load(tr_name, path);
+
+    if(!res)
+        qDebug() << Q_FUNC_INFO << "translation" << tr_name << "not found in" << path;
+
+    return res;
+}
+
+void TranslationLoader::loadApplicationTranslations(const QStringList& paths) {
+    qDebug() << Q_FUNC_INFO << "load application translations from paths:" << paths;
+
+    foreach(QString path, paths) {
+        if(loadApplicationTranslation(path))
+            break;
+    }
+}
+
+QTranslator * TranslationLoader::systemTranslator() {
+    return &system_;
+}
--- gui/translationloader.h
+++ gui/translationloader.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Serge Poltavsky                                 *
+ *   Copyright (C) 2011 by Serge Poltavsky                                 *
  *   serge.poltavski@gmail.com                                             *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -16,36 +16,32 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#include <QApplication>
+#ifndef TRANSLATIONLOADER_H
+#define TRANSLATIONLOADER_H
+
+#include <QTranslator>
 #include <QStringList>
-#include "mainwindow.h"
-#include "translationloader.h"
+#include <QLocale>
 
-int main(int argc, char * argv[]) {
-#ifdef Q_WS_X11
-    QApplication::setGraphicsSystem("raster");
-#endif
+class TranslationLoader
+{
+public:
+    TranslationLoader();
 
-    QApplication app(argc, argv);
+    QTranslator * applicationTranslator();
+    QTranslator * systemTranslator();
 
-    TranslationLoader loader;
-    loader.load();
-    app.installTranslator(loader.systemTranslator());
-    app.installTranslator(loader.applicationTranslator());
+    void load();
+private:
+    void loadApplicationTranslation();
+    bool loadApplicationTranslation(const QString& path);
+    void loadApplicationTranslations(const QStringList& paths);
+    void loadSystemTranslation();
+private:
+    QLocale locale_;
+private:
+    static QTranslator app_;
+    static QTranslator system_;
+};
 
-    app.setOrganizationName("openocr.org");
-    app.setApplicationName("Quneiform OCR");
-    app.setApplicationVersion("0.0.1");
-    MainWindow w;
-    w.show();
-
-    if(argc > 1) {
-        QStringList files;
-        for(int i = 1; i < argc; i++)
-            files << QString::fromLocal8Bit(argv[i]);
-
-        w.openImages(files);
-    }
-
-    return app.exec();
-}
+#endif // TRANSLATIONLOADER_H
