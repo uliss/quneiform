5275c0253e49e7701d393989bef0b2ae241a0816
Serge Poltavski <serge.poltavski@gmail.com>
2011-06-01 18:38:11 +0400

    Uninstall target added.
    Type make uninstall - to remove installed files.

---
+++ cmake/uninstall.cmake
@@ -0,0 +1,8 @@
+# uninstall target
+configure_file(
+    "${CMAKE_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+    "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
+    IMMEDIATE @ONLY)
+
+add_custom_target(uninstall
+    COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/cmake_uninstall.cmake)
