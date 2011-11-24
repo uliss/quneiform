88d752090d79ff9ed8abef75be84ef19424fb816
Serge Poltavski <serge.poltavski@gmail.com>
2011-11-25 01:45:29 +0300

    Unused definitions removed

--- src/rmarker/rmarker.cpp
+++ src/rmarker/rmarker.cpp
@@ -152,22 +152,6 @@ void RMarker::setOptions(const RecognizeOptions& opts) {
 
 }
 
-#define INCLINE_FACTOR  2048
-
-#define IDEAL_XY(x, y)   \
-         {\
-             y = (int16_t) (y - (int32_t) x * nIncline / INCLINE_FACTOR);\
-             x = (int16_t) (x + (int32_t) y * nIncline / INCLINE_FACTOR);\
-         }
-
-#define REAL_XY(x, y)   \
-         {\
-             x = (int16_t) (x - (int32_t) y * nIncline / INCLINE_FACTOR);\
-             y = (int16_t) (y + (int32_t) x * nIncline / INCLINE_FACTOR);\
-        }
-
-#define TYPE_FON      CPAGE_GetInternalType("TYPE_FON")
-
 extern Handle DebugFill;
 extern Handle hVertCells;
 extern Handle hNegaCells;
@@ -197,7 +181,6 @@ extern Handle hFon;
 extern Handle hEnd;
 
 static uint32_t gwRC = 0;
-Bool dpDebugUpDown;
 
 Bool32 PageMarkup(PRMPreProcessImage Image)
 {
