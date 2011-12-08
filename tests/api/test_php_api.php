fe35e3d8ec61fff62952aeca3739a520afefa125
Serge Poltavski <serge.poltavski@gmail.com>
2011-12-09 03:33:13 +0400

    PHP api test skeleton added

---
+++ tests/api/test_php_api.php
@@ -0,0 +1,17 @@
+<?php
+
+require '@CMAKE_BINARY_DIR@/api/swig/php/cuneiform.php';
+
+echo "PHP api test\n";
+echo "version: " . VERSION . "\n";
+echo "build:   " . BUILD_NUMBER . "\n";
+
+$fopts = new cuneiform_format_options;
+$ropts = new cuneiform_recognition_options;
+
+$ropts->set_language(LANGUAGE_ENGLISH);
+
+$page = cuneiform::recognize("@CMAKE_SOURCE_DIR@/images/quneiform/english.png", $ropts, $fopts);
+$page->save("/dev/stdout", FORMAT_TEXT, $fopts);
+
+?>
