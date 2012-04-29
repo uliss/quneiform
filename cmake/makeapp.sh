28e1ef1af10e0e440c58df989af88d0ec6eada16
Serge Poltavski <serge.poltavski@gmail.com>
2012-04-30 00:09:20 +0400

    dmg improvements

--- cmake/makeapp.sh
+++ cmake/makeapp.sh
@@ -83,8 +83,73 @@ rm -rf Quneiform.dmg "${DESTDIR}/Quneiform-${VERSION}.dmg"
 
 # DMG creation
 cd $DESTDIR
+rm -rf tmp.dmg
 
 echo "Creating DMG..."
 macdeployqt Quneiform.app -dmg -no-plugins -verbose=0
-mv Quneiform.dmg Quneiform-$VERSION.dmg
+#mv Quneiform.dmg Quneiform-$VERSION.dmg
+
+echo "Converting to RW format"
+hdiutil convert Quneiform.dmg -format UDRW -o tmp.dmg
+rm Quneiform.dmg
+
+DMGSIZE=$(hdiutil resize -limits tmp.dmg  | tail -1 | cut -f 1)
+let DMGSIZE+=10000
+echo "Resize DMG-image to $DMGSIZE"
+hdiutil resize -sectors $DMGSIZE tmp.dmg
+
+echo "Mount DMG..."
+device=$(hdiutil attach -readwrite -noverify -noautoopen tmp.dmg | egrep '^/dev/' | sed 1q | awk '{print $1}')
+mountdir=$(hdiutil info | grep $device | grep Volumes | cut -f 3)
+echo "Mount $device at $mountdir"
+title=$(basename $mountdir)
+
+sleep 2
+
+cp "$SRCDIR/gui/resources/dmg_background.png" "$mountdir/logo.png"
+cp "$SRCDIR/gui/resources/disk_logo.icns" "$mountdir/.VolumeIcon.icns"
+SetFile -c icnC "$mountdir/.VolumeIcon.icns"
+SetFile -a C "$mountdir"
+
+echo '
+on run argv
+   tell application "Finder"
+     tell disk "'${title}'"
+           open
+           set current view of container window to icon view
+           set toolbar visible of container window to false
+           set statusbar visible of container window to false
+           set the bounds of container window to {400, 100, 900, 500}
+           set theViewOptions to the icon view options of container window
+           set arrangement of theViewOptions to not arranged
+           set icon size of theViewOptions to 72
+           make new alias file at container window to POSIX file "/Applications" with properties {name:"Applications"}
+           set position of item "'Quneiform'" of container window to {100, 100}
+           set position of item "Applications" of container window to {400, 100}
+           set background picture of theViewOptions to file "logo.png"
+           close
+           delay 1
+           open
+           delay 1
+           close
+           do shell script "SetFile -a V \"'$mountdir'/logo.png\""
+           eject "'${title}'"
+     end tell
+   end tell
+end run
+' | osascript
+
+sync
+sync
+
+chmod -Rf go-w /Volumes/"${title}"
+hdiutil detach ${device}
+
+sleep 1
+
+echo "Compressing DMG..."
+
+hdiutil convert tmp.dmg -format UDZO -imagekey zlib-level=9 -o "Quneiform-${VERSION}.dmg"
+rm -rf tmp.dmg
+
 
