92f8d700e23b1fd275c76d10b69bd7d5d70151fe
Serge Poltavski <serge.poltavski@gmail.com>
2012-05-15 15:56:48 +0400

    Boost NSIS fix

--- cmake/make_package.cmake
+++ cmake/make_package.cmake
@@ -21,7 +21,7 @@ if(WIN32)
         endif()
     endmacro()
 endif()
-
+ 
 if(APPLE)
     add_custom_target(dmg COMMAND "${CMAKE_SOURCE_DIR}/cmake/makeapp.sh"
                                   "${CMAKE_BINARY_DIR}" # dest dir
@@ -84,8 +84,24 @@ elseif(WIN32)
         cf_install_dll(zlib1 ${ZLIB_INCLUDE_DIRS}/../bin)
     endif()
     
+    # install FreeImage
     if(CF_USE_IMAGE_LIBRARY_FREEIMAGE)
         cf_install_dll(FreeImage ${FREEIMAGE_INCLUDE_PATH})
+    endif()
+    
+    # install boost
+    if(Boost_SERIALIZATION_FOUND AND CF_EXPORT_LIBTYPE STREQUAL "SHARED")
+        set(boost_dll "")
+        
+        if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+            set(boost_lib_path ${Boost_SERIALIZATION_LIBRARY_DEBUG})
+        else()
+            set(boost_lib_path ${Boost_SERIALIZATION_LIBRARY_RELEASE})
+        endif()
+        
+        get_filename_component(boost_dll "${boost_lib_path}" NAME_WE)
+        #message(STATUS ${Boost_LIBRARY_DIRS})
+        cf_install_dll(${boost_dll} ${Boost_LIBRARY_DIRS})
     endif()
 	
 	if(MINGW)
