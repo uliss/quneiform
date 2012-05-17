d3332559dd38fad40b9da13fba174432dc2d7bd9
Serge Poltavski <serge.poltavski@gmail.com>
2012-05-17 21:18:32 +0400

    CCOM cmake cleanup

--- cmake/code_check.cmake
+++ cmake/code_check.cmake
@@ -2,6 +2,6 @@ include(CppcheckTargets)
 
 function(cf_cppcheck name)
     if(TESTS)
-        add_cppcheck(${name} UNUSED_FUNCTIONS STYLE POSSIBLE_ERROR)
+        add_cppcheck(${name} UNUSED_FUNCTIONS STYLE POSSIBLE_ERROR FORCE)
     endif()
 endfunction()
