0e57cdfaf23c3bf3cfc68b01e9e27c9df599a957
Serge Poltavski <serge.poltavski@gmail.com>
2013-04-25 14:46:44 +0400

    russian tr added

---
+++ contrib/libsanewidget/src/i18n/sanewidget_ru.ts
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>LabeledGamma</name>
+    <message>
+        <source>Brightness</source>
+        <translation>Яркость</translation>
+    </message>
+    <message>
+        <source>Contrast</source>
+        <translation>Контрастность</translation>
+    </message>
+    <message>
+        <source>Gamma</source>
+        <translation>Гамма</translation>
+    </message>
+</context>
+<context>
+    <name>PreviewArea</name>
+    <message>
+        <source>Zoom In</source>
+        <translation>Увеличить</translation>
+    </message>
+    <message>
+        <source>Zoom Out</source>
+        <translation>Уменьшить</translation>
+    </message>
+    <message>
+        <source>Zoom to Selection</source>
+        <translation>Уместить выделенное</translation>
+    </message>
+    <message>
+        <source>Zoom to Fit</source>
+        <translation>Уместить</translation>
+    </message>
+</context>
+<context>
+    <name>SaneWidget</name>
+    <message>
+        <source>Select Scanner</source>
+        <translation>Выбрать сканер</translation>
+    </message>
+    <message>
+        <source>Test Scanner</source>
+        <translation>Тестовый сканер</translation>
+    </message>
+    <message>
+        <source>Zoom In</source>
+        <translation>Увеличить</translation>
+    </message>
+    <message>
+        <source>Zoom Out</source>
+        <translation>Уменьшить</translation>
+    </message>
+    <message>
+        <source>Zoom to Selection</source>
+        <translation>Уместить выделенное</translation>
+    </message>
+    <message>
+        <source>Zoom to Fit</source>
+        <translation>Уместить</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Предпросмотр</translation>
+    </message>
+    <message>
+        <source>Final Scan</source>
+        <translation>Сканирование</translation>
+    </message>
+</context>
+</TS>
