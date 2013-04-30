ef8e9f5e3de06f6f0f7f8b9f0ac13e7879159528
Serge Poltavski <serge.poltavski@gmail.com>
2013-04-30 09:16:30 +0400

    define remove

--- gui/widgets/mainwindow.cpp
+++ gui/widgets/mainwindow.cpp
@@ -60,9 +60,9 @@
 #include "settingskeys.h"
 #include "scan/scannerdialog.h"
 
-#ifndef Q_WS_MAC
-#define DISABLE_SCANNER_MENU
-#endif
+//#ifndef Q_WS_MAC || Q_OS_LINUX
+//#define DISABLE_SCANNER_MENU
+//#endif
 
 static const int VERSION_MAJOR = 0;
 static const int VERSION_MINOR = 0;
